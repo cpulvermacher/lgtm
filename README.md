a8e982ebae52189f7e8ce45b0dea0dcb823d9688
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2024-08-01 01:02:39 +0900

    a working chat extension

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AI-review
+
+## References
+- https://code.visualstudio.com/api/extension-guides/chat
+- https://code.visualstudio.com/api/extension-guides/language-model
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,25 @@
 import * as vscode from 'vscode';
 
-
 // called the first time a command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate() {
+    vscode.chat.createChatParticipant('ai-reviewer', handler);
 }
 
 export function deactivate() {
     /* nothing to do here */
 }
 
+const handler: vscode.ChatRequestHandler = async (
+    request: vscode.ChatRequest,
+    context: vscode.ChatContext,
+    stream: vscode.ChatResponseStream
+    // token: vscode.CancellationToken
+): Promise<void> => {
+    console.log('Received request:', request, 'with context:', context);
+
+    if (request.command === 'review') {
+        stream.markdown(
+            "Awesome! Let's review your code. Which commit would you like me to review?"
+        );
+    }
+};
