06401ac1fd12bb47331e67feab336ecf2d8d9271
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2024-08-02 00:47:06 +0900

    talk to the language model

--- src/extension.ts
+++ src/extension.ts
@@ -12,8 +12,8 @@ export function deactivate() {
 const handler: vscode.ChatRequestHandler = async (
     request: vscode.ChatRequest,
     context: vscode.ChatContext,
-    stream: vscode.ChatResponseStream
-    // token: vscode.CancellationToken
+    stream: vscode.ChatResponseStream,
+    token: vscode.CancellationToken
 ): Promise<void> => {
     console.log('Received request:', request, 'with context:', context);
 
@@ -21,5 +21,41 @@ const handler: vscode.ChatRequestHandler = async (
         stream.markdown(
             "Awesome! Let's review your code. Which commit would you like me to review?"
         );
+
+        const prompt = [
+            vscode.LanguageModelChatMessage.User(
+                'How to print meow using Spring Boot?' + request.prompt
+            ),
+        ];
+        //TODO can select gpt-4o, but get strange exception when reading response
+        // const models = await vscode.lm.selectChatModels({ family: 'gpt-4o' });
+        const models = await vscode.lm.selectChatModels({});
+        console.log('Selected models:', models);
+
+        if (models.length === 0) {
+            stream.markdown('No models found');
+            return;
+        }
+
+        const model = models[0];
+        let response: vscode.LanguageModelChatResponse;
+        try {
+            response = await model.sendRequest(prompt, {}, token);
+        } catch (e) {
+            if (e instanceof vscode.LanguageModelError) {
+                stream.markdown(`Error: ${e.message}`);
+            } else {
+                console.error('Error:', e);
+            }
+            return;
+        }
+
+        try {
+            for await (const fragment of response.text) {
+                stream.markdown(fragment);
+            }
+        } catch (e) {
+            stream.markdown(`Error: ${e}`);
+        }
     }
 };
