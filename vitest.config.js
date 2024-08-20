30128d8cd73a69ffd0918a017022dfa56e96dc7c
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2024-08-20 22:27:09 +0900

    remove vitest

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-    test: {
-        include: ['src/test/unit/**/*.test.ts']
-    },
-});
