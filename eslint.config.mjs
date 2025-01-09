4909029299b786a1a17b5790f3a9012a2e34abf1
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2025-01-10 01:47:42 +0900

    simplify eslint config & use unified typescript-eslint package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,33 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import eslint from "@eslint/js";
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
-    ignores: ["**/out", "**/dist", "**/*.d.ts"],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-).map(config => ({
-    ...config,
-    files: ["**/*.ts"],
-})), {
-    files: ["**/*.ts"],
-
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
+export default tseslint.config(
+    {
+        ignores: [
+            '.vscode-test',
+            'out',
+            'dist',
+        ]
     },
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 6,
-        sourceType: "module",
-    },
-
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        "no-restricted-imports": ["warn", {
-            paths: [{
-                // allowed only in extension.ts and config.ts, since it cannot be imported in unit tests.
-                name: 'vscode',
-                message: "Importing 'vscode' is restricted except for type imports.",
-                importNames: ['default'],
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            "@typescript-eslint/naming-convention": ["warn", {
+                selector: "import",
+                format: ["camelCase", "PascalCase"],
             }],
-        }]
-    },
-}];
+            curly: "warn",
+            eqeqeq: "warn",
+            "no-throw-literal": "warn",
+            "no-restricted-imports": ["warn", {
+                paths: [{
+                    // allowed only in extension.ts and config.ts, since it cannot be imported in unit tests.
+                    name: 'vscode',
+                    message: "Importing 'vscode' is restricted except for type imports.",
+                    // TODO only restricts default import, so model.ts is already broken
+                    importNames: ['default'],
+                }],
+            }]
+        },
+    })
