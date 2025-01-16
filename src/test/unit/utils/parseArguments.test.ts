30f4980cf473031e7e35a03c5b45f7f3a1212788
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2025-01-16 12:03:56 +0900

    chore: more tests

--- src/test/unit/utils/parseArguments.test.ts
+++ src/test/unit/utils/parseArguments.test.ts
@@ -18,17 +18,40 @@ describe('parseArguments', () => {
         const result = await parseArguments(mockGit, 'target');
 
         expect(result).toEqual({ target: 'target', base: undefined });
+
+        expect(mockGit.getCommitRef).toHaveBeenCalledTimes(1);
     });
 
     it('parses two arguments', async () => {
         const result = await parseArguments(mockGit, 'target base');
 
         expect(result).toEqual({ target: 'target', base: 'base' });
+
+        expect(mockGit.getCommitRef).toHaveBeenCalledTimes(2);
     });
 
     it('parses two arguments with whitespace', async () => {
         const result = await parseArguments(mockGit, ' target base ');
 
         expect(result).toEqual({ target: 'target', base: 'base' });
+    });
+
+    it('throws for invalid target', async () => {
+        vi.mocked(mockGit.getCommitRef).mockRejectedValue(
+            new Error('not found')
+        );
+
+        await expect(parseArguments(mockGit, 'target')).rejects.toThrow();
+    });
+
+    it('throws for invalid base', async () => {
+        vi.mocked(mockGit.getCommitRef).mockResolvedValueOnce('ignored');
+        vi.mocked(mockGit.getCommitRef).mockRejectedValueOnce(
+            new Error('not found')
+        );
+
+        await expect(parseArguments(mockGit, 'target base')).rejects.toThrow();
+
+        expect(mockGit.getCommitRef).toHaveBeenCalledTimes(2);
     });
 });
